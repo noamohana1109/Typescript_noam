87cfc4c0bf1f2f3afe48fc93e48fdf559429ccf7
Oren  Ohana <orenoh@aeldom.group>
2023-01-11 21:05:40 +0200

    part1

---
+++ index.ts
@@ -0,0 +1,12 @@
+function getAverage(array:number[])
+{
+    let count=0;
+    let sum=0;
+for(let i=0;i<array.length;i++)
+{
+    sum=+array[i];
+    let count=++
+}
+alert(sum/count);
+}
+getAverage();
